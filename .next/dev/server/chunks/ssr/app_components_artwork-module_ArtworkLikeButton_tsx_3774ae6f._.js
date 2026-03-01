2f7f8eeaad72db64dccea02bc633d143ecd92a48
lukaarakic <lukarakic0@protonmail.com>
2026-03-01 01:29:05 +0100

    fix: permanently remove .next from version control

--- .next/dev/server/chunks/ssr/app_components_artwork-module_ArtworkLikeButton_tsx_3774ae6f._.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-"[project]/app/components/artwork-module/ArtworkLikeButton.tsx [app-ssr] (ecmascript)", ((__turbopack_context__, module, exports) => {
-
-const e = new Error("Could not parse module '[project]/app/components/artwork-module/ArtworkLikeButton.tsx'\n\n'import', and 'export' cannot be used outside of module code");
-e.code = 'MODULE_UNPARSABLE';
-throw e;
-}),
-];
--- .next/dev/static/chunks/app_(app)_play_page_tsx_8af5ecc9._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_996a8a50._.js"
-],
-    source: "dynamic"
-});
